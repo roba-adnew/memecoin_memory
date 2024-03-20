02ba7114490d5a46bf2de63b7b5f7d27e52d4f7a
roba <10408936+roba-adnew@users.noreply.github.com>
2024-03-20 15:12:31 -0400

    Upgraded dialog to toggle if the user has lost or if they want to continue

--- src/EndCard/EndCard.jsx
+++ src/EndCard/EndCard.jsx
@@ -1,23 +1,42 @@
+import { useRef } from 'react'
 import PropTypes from 'prop-types'
 import './EndCard.css'
 
 function EndCard({ hasLost, setHasLost }) {
 
+    const dialogRef = useRef(null);
+
+    function toggleDialog() {
+        if (!dialogRef.current) {
+            return
+        }
+
+        dialogRef.current.hasAttribute('open') ?
+            dialogRef.current.close() :
+            dialogRef.current.showModal();
+
+    }
+
     return (
         <>
-            {hasLost && (
-                <dialog className={hasLost ? 'open' : 'close'}>
-                    <div>Not bad, want to try again?</div>
-                    <button onClick={() => setHasLost(false)}>Yeah, let&apos;s try again</button>
-                </dialog>
-            )}
+            <dialog ref={dialogRef}>
+                <div>Not bad, want to try again?</div>
+                <button
+                    onClick={() => {
+                        toggleDialog();
+                        setHasLost(false);
+                    }}
+                >
+                    Yeah, let&apos;s try again</button>
+            </dialog>
+            {hasLost && dialogRef.current.showModal()}
         </>
     )
 }
 
-EndCard.propTypes = { 
-    hasLost : PropTypes.bool,
-    setHasLost : PropTypes.func,
- }
+EndCard.propTypes = {
+    hasLost: PropTypes.bool,
+    setHasLost: PropTypes.func,
+}
 
 export default EndCard;
